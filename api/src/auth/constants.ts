131ea429e22d5eecb98dc9520ef15384750922b9
Lucas Rocha <13350752+lucascudo@users.noreply.github.com>
2022-04-19 09:52:52 +0100

    adds .env for environment configuration

--- api/src/auth/constants.ts
+++
@@ -1,4 +0,0 @@
-export const jwtConstants = {
-    //TODO. change it and move it to .env
-    secret: 'eyJudEGiOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIzNTM0NTQzNTQzNTQzNTM0NTMiLCJleHAiOjE1MDQ2OTkyNTZ9.zG-2FvGegujxoLWwIQfNB5IT46D-x64e8dADYwi6aRM',
-};
