315d9bd8fde5fd519abd8334bd654394ea0c9da1
Lucas Rocha <lucas.teixeirarocha@capgemini.com>
2022-06-01 09:47:50 +0100

    finishes HTTPS implementation

--- README.md
+++ README.md
@@ -2,8 +2,32 @@
 
 ![System Architecture](https://raw.githubusercontent.com/lucascudo/PrivacyEnhancedPrizes/main/PrivacyEnhancedPrizes.png)
 
-## TO DO
-### extras
+## API SETUP
+```
+cd api
+npm install
+cd secrets
+openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout key.pem -out cert.pem
+``` 
+## CLIENT_APP SETUP
+```
+cd clientapp
+npm install
+cd secrets
+openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout key.pem -out cert.pem
+```
+ ## RUN API
+```
+cd api
+npm run start:dev
+``` 
+ ## RUN CLIENT_APP
+```
+cd clientapp
+npm run start:ssl
+``` 
+
+### extras (TODO)
 - embelezar as páginas
 - adicionar comentários swagger (documentação da API)
 - corrigir testes unitários
--- api/src/main.ts
+++ api/src/main.ts
@@ -4,22 +4,30 @@ import { AppModule } from './app.module';
 import * as fs from 'node:fs';
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync('./secrets/key.pem'),
-    cert: fs.readFileSync('./secrets/cert.pem'),
-  };
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions,
-  });
-  const swaggerConfig = new DocumentBuilder()
-    .setTitle('PrivacyEnhancedPrizes')
-    .setDescription('The PrivacyEnhancedPrizes API description')
-    .setVersion('1.0')
-    .addTag('PrivacyEnhancedPrizes')
-    .build();
-  const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, document);
-  app.enableCors();
-  await app.listen(3000);
+  let app;
+  let httpsOptions = {};
+  try {
+    httpsOptions = {
+      key: fs.readFileSync('./secrets/key.pem'),
+      cert: fs.readFileSync('./secrets/cert.pem'),
+    };
+    app = await NestFactory.create(AppModule, {
+      httpsOptions,
+    });
+  } catch (e) {
+    console.error(e);
+    app = await NestFactory.create(AppModule);
+  } finally {
+    const swaggerConfig = new DocumentBuilder()
+      .setTitle('PrivacyEnhancedPrizes')
+      .setDescription('The PrivacyEnhancedPrizes API description')
+      .setVersion('1.0')
+      .addTag('PrivacyEnhancedPrizes')
+      .build();
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('api', app, document);
+    app.enableCors();
+    await app.listen(3000);
+  }
 }
 bootstrap();
