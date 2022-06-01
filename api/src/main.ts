da7a0d71097d3ba5c23a4e057ee6e3eca18a1912
Lucas Rocha <lucas.teixeirarocha@capgemini.com>
2022-06-01 09:29:32 +0100

    adds https to the backend

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,16 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as fs from 'node:fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
+  const httpsOptions = {
+    key: fs.readFileSync('./secrets/key.pem'),
+    cert: fs.readFileSync('./secrets/cert.pem'),
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
+  const swaggerConfig = new DocumentBuilder()
     .setTitle('PrivacyEnhancedPrizes')
     .setDescription('The PrivacyEnhancedPrizes API description')
     .setVersion('1.0')
     .addTag('PrivacyEnhancedPrizes')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
   app.enableCors();
   await app.listen(3000);
